42200cfbc6c00d4af8bf4e09362f8194a1e234f7
code1719 <codyhearell@gmail.com>
2024-01-25 09:29:32 -0700

    part 1

---
+++ src/components/RecordList.jsx
@@ -0,0 +1,101 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Loader from "./Loader/Loader";
+
+const Record = (props) => (
+  <tr>
+    <td>{props.record.firstName}</td>
+    <td>{props.record.lastName}</td>
+    <td>{props.record.email}</td>
+    <td>{props.record.age}</td>
+    <td>{props.record.currentCollege}</td>
+    <td>
+      <Link className="btn btn-link" to={`/edit/${props.record._id}`}>
+        Edit
+      </Link>
+      |
+      <button
+        className="btn btn-link"
+        onClick={() => {
+          props.deleteRecord(props.record._id);
+        }}
+      >
+        Delete
+      </button>
+    </td>
+  </tr>
+);
+
+export default function RecordList() {
+  const [loading, setLoading] = useState(true);
+  const [records, setRecords] = useState([]);
+
+  // This method fetches the records from the database.
+  useEffect(() => {
+    setLoading(true);
+    async function getRecords() {
+      try {
+        const response = await fetch(
+          `mongodb+srv://codyhearell:dyOqv3eShCDh6Vvx@apiproject.r3ltm2o.mongodb.net/Students`
+        );
+        if (!response.ok) {
+          throw new Error(`An error occurred: ${response.statusText}`);
+        }
+        const records = await response.json();
+        setRecords(records);
+      } catch (error) {
+        window.alert(error.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+    getRecords();
+  }, []);
+
+  // This method will delete a record
+  async function deleteRecord(id) {
+    try {
+      await fetch(
+        `mongodb+srv://codyhearell:dyOqv3eShCDh6Vvx@apiproject.r3ltm2o.mongodb.net/Students/${id}`,
+        {
+          method: "DELETE",
+        }
+      );
+      const newRecords = records.filter((el) => el._id !== id);
+      setRecords(newRecords);
+    } catch (error) {
+      window.alert(`An error occurred while deleting: ${error.message}`);
+    }
+  }
+
+  // This method will map out the records on the table
+  function recordList() {
+    return records.map((record) => (
+      <Record
+        record={record}
+        deleteRecord={() => deleteRecord(record._id)}
+        key={record._id}
+      />
+    ));
+  }
+
+  // This following section will display the table with the records of individuals.
+  return (
+    <div className="container">
+      <h3 className="contact-title">Contact List</h3>
+      <table className="table table-striped" style={{ marginTop: 20 }}>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Age</th>
+            <th>Current College</th>
+            <th>Modify Student</th>
+          </tr>
+        </thead>
+        <tbody>{loading ? <Loader /> : recordList()}</tbody>
+      </table>
+    </div>
+  );
+}
